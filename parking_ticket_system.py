6249f67643c36a73dbe08655577840ec4e67cea1
jemodzel <jeff.modzel@ellucian.com>
2019-03-18 15:16:46 -0400

    Update parking ticket system

--- parking_ticket_system.py
+++ parking_ticket_system.py
@@ -21,14 +21,17 @@ def main():
         print('Generated Parking Ticket for {first} {last}, Amount ${amount}, License Plate: {plate}'
             .format(first=t['firstName'],last=t['lastName'],amount=t['amount'],plate=t['licensePlate']))
 
-        publish_ticket_to_ethos(ethos, t)
+        cn = convert_ticket_to_change_notification(t)
+        print('Publishing change-notification')
+        print(cn)
+        ethos.send_change_notification(cn)
 
         wait_seconds = random.randint(1,20)
-        print('Waiting for {seconds} seconds...\n'.format(seconds=wait_seconds))
+        print('\nWaiting for {seconds} seconds...\n'.format(seconds=wait_seconds))
         time.sleep(wait_seconds)
 
 def generate_random_parking_ticket():
-    ticket = create_blank_parking_ticket()
+    ticket = {'parkingTicketId':'', 'firstName' : '', 'lastName': '', 'licensePlate': '', 'amount': 0}
 
     ticket['firstName'] = random.choice(['Billy','Joey','Jen','Taylor','Marissa','Clay','Chris','Nick','Keith','Wyatt','Christian','Priya'])
     ticket['lastName'] = random.choice(['Smith','Johnson','Williams','Brown','Jones','Miller','Davis','Garcia','Rodriguez','Wilson'])
@@ -38,24 +41,16 @@ def generate_random_parking_ticket():
 
     return ticket
 
-def create_blank_parking_ticket():
-    return {'parkingTicketId':'', 'firstName' : '', 'lastName': '', 'licensePlate': '', 'amount': 0}
-
-def create_blank_change_notification():
-    return { 'resource' : {'name':'', 'id':''},'operation': '','contentType':'','content': None}
-
-def publish_ticket_to_ethos(ethos, parking_ticket):
-    print('Publishing change-notification to Ethos Integration')
-    change_notification = create_blank_change_notification()
+def convert_ticket_to_change_notification(ticket):
+    change_notification = { 'resource' : {'name':'', 'id':''},'operation': '','contentType':'','content': None}
 
     change_notification['operation'] = 'created'
     change_notification['contentType'] = 'resource-representation'
     change_notification['resource']['name'] = 'parking-tickets'
-    change_notification['resource']['id'] = parking_ticket['parkingTicketId']
-    change_notification['content'] = parking_ticket
+    change_notification['resource']['id'] = ticket['parkingTicketId']
+    change_notification['content'] = ticket
 
-    print(change_notification)
-    ethos.send_change_notification(change_notification)
+    return change_notification
 
 if __name__ == '__main__':
     main()
